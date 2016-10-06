ab7386c525a2ec7d3da21f33de389940aaf3275d
Mike Gordanier <gord0213@algonquinlive.com>
2016-10-06 00:08:15 -0400

    Finished Lab 3 Final

--- app/src/main/java/com/example/mike/lab2/ListItemsActivity.java
+++ app/src/main/java/com/example/mike/lab2/ListItemsActivity.java
@@ -1,5 +1,6 @@
 package com.example.mike.lab2;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -16,15 +17,17 @@ import android.widget.Toast;
 
 public class ListItemsActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "ListItemsActivity";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(ACTIVITY_NAME, "is onCreate");
+        System.out.print("onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);//GridLayout);//
         ImageButton button = (ImageButton) findViewById(R.id.imageButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -55,9 +58,7 @@ public class ListItemsActivity extends AppCompatActivity {
             }
         });
 
-
         }
-    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
@@ -69,30 +70,35 @@ public class ListItemsActivity extends AppCompatActivity {
     }
     @Override
     protected void onResume(){
-        super.onResume();
         Log.i(ACTIVITY_NAME, "is onResume");
+        System.out.print("onResume");
+        super.onResume();
     }
 
     @Override
     protected void onStart(){
-        super.onStart();
         Log.i(ACTIVITY_NAME, "is onStart");
+        System.out.print("onStart");
+        super.onStart();
 
     }
     @Override
     protected void onPause(){
-        super.onPause();
         Log.i(ACTIVITY_NAME, "is onPause");
+        System.out.print("onPause");
+        super.onPause();
     }
     @Override
     protected void onStop(){
-        super.onStop();
         Log.i(ACTIVITY_NAME, "is onStop");
+        System.out.print("onStop");
+        super.onStop();
     }
     @Override
     protected void onDestroy(){
-        super.onDestroy();
         Log.i(ACTIVITY_NAME, "is onDestroy");
+        System.out.print("onDestroy");
+        super.onDestroy();
     }
 
     public void onCheckboxClicked(View view){
@@ -102,7 +108,11 @@ public class ListItemsActivity extends AppCompatActivity {
                 .setTitle(R.string.dialog_title)
                 .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        Intent resultIntent = new Intent(  );
+                        resultIntent.putExtra("Response", "My information to share");
+                        setResult(Activity.RESULT_OK, resultIntent);
                         finish();
+
                     }
                 }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
